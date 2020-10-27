325b59dbe172b7dbcf0c2aa5a050f74c09c3e274
Dwi Ariyanto <dwi.ariyanto@jakmall.com>
2020-10-27 10:07:01 +0700

    feat(screen): implement code push on app screen

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
+import CodePush from 'react-native-code-push';
 
 import {
   Header,
@@ -25,7 +26,77 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      log: "",
+    }
+    console.log('constructor ', this.state);
+  
+  }
+
+  componentDidMount() {
+
+    CodePush.getUpdateMetadata(CodePush.UpdateState.RUNNING).then((update) => {
+      this.updateLog('getUpdateMetadata RUNNING ' + update);
+      console.log('getUpdateMetadata RUNNING', update);
+    });
+
+    CodePush.getUpdateMetadata(CodePush.UpdateState.RELEASE).then((update) => {
+      this.updateLog('getUpdateMetadata RELEASE ' + update);
+      console.log('getUpdateMetadata RELEASE', update);
+    });
+
+    CodePush.getUpdateMetadata(CodePush.UpdateState.PENDING).then((update) => {
+      this.updateLog('getUpdateMetadata PENDING ' + update);
+      console.log('getUpdateMetadata PENDING', update);
+    });
+  }
+
+  codePushStatusDidChange(status) {
+    switch (status) {
+      case CodePush.SyncStatus.CHECKING_FOR_UPDATE:
+        this.updateLog('Checking for updates.');
+        console.log('Checking for updates.');
+        break;
+      case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
+        this.updateLog('Downloading package.');
+        console.log('Downloading package.');
+        break;
+      case CodePush.SyncStatus.INSTALLING_UPDATE:
+        this.updateLog('Installing update.');
+        console.log('Installing update.');
+        break;
+      case CodePush.SyncStatus.UP_TO_DATE:
+        this.updateLog('Up-to-date.');
+        console.log('Up-to-date.');
+        break;
+      case CodePush.SyncStatus.UPDATE_INSTALLED:
+        this.updateLog('Update installed.');
+        console.log('Update installed.');
+        break;
+    }
+  }
+
+  updateLog(message) {
+    var log = this.state.log;
+    log = log + '\n' + message;
+    this.setState({
+      log: log,
+    });
+  }
+
+  codePushDownloadDidProgress(progress) {
+    this.updateLog(progress.receivedBytes + ' of ' + progress.totalBytes + ' received.');
+    console.log(
+      progress.receivedBytes + ' of ' + progress.totalBytes + ' received.',
+    );
+  }
   render() {
+    var number1 = 1
+    var number2 = 2
+    var result = number1 < number2 ? number2 : number1
+
     return (
       <>
         <StatusBar barStyle="dark-content" />
@@ -33,39 +104,18 @@ class App extends Component {
           <ScrollView
             contentInsetAdjustmentBehavior="automatic"
             style={styles.scrollView}>
-            <Header />
-            {global.HermesInternal == null ? null : (
-              <View style={styles.engine}>
-                <Text style={styles.footer}>Engine: Hermes</Text>
-              </View>
-            )}
             <View style={styles.body}>
               <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Step One</Text>
-                <Text style={styles.sectionDescription}>
-                  Edit <Text style={styles.highlight}>App.js</Text> to change
-                  this screen and then come back to see your edits.
-                </Text>
+                <Text style={styles.sectionTitle}>Versi 1.0.0</Text>
+                <Text style={styles.sectionDescription}>Hallo</Text>
+                <Text style={styles.sectionDescription}>if {number1} {"<"} {number2} then show {result} </Text>
+                <View
+                  style={{
+                    height: 100,
+                  }}
+                />
+                <Text style={styles.sectionDescription}>{this.state.log}</Text>
               </View>
-              <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>See Your Changes</Text>
-                <Text style={styles.sectionDescription}>
-                  <ReloadInstructions />
-                </Text>
-              </View>
-              <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Debug</Text>
-                <Text style={styles.sectionDescription}>
-                  <DebugInstructions />
-                </Text>
-              </View>
-              <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Learn More</Text>
-                <Text style={styles.sectionDescription}>
-                  Read the docs to discover what to do next:
-                </Text>
-              </View>
-              <LearnMoreLinks />
             </View>
           </ScrollView>
         </SafeAreaView>
@@ -113,4 +163,9 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+MyApp = CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+  installMode: CodePush.InstallMode.ON_NEXT_RESUME,
+})(App);
+
+export default MyApp;
